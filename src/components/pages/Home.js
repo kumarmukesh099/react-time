5670267495812ebb15a72fdd1c3f00146514b3b7
mukesh kumar <mk380345@gmail.com>
2020-10-08 21:57:03 +0530

    added image in home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,16 @@
-import React, { Fragment } from 'react'
+import React, { Fragment , useContext} from 'react'
 import Users from '../users/Users';
 import Search from '../users/Search';
+import homeImage from '../layout/searchimage.png';
+import GithubContext from '../../context/github/githubContext';
 
 const Home = () => {
+    const githubContext = useContext(GithubContext);
     return (
         <Fragment>
             <Search />  
             <Users />
+            {!githubContext.users.length && <img src={homeImage} alt="" />}
         </Fragment>
     )
 }
