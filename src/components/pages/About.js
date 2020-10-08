039434a79bd623db1650c5ecccbfc1925a213263
mukesh kumar <mk380345@gmail.com>
2020-10-08 21:41:50 +0530

    Minor Fix

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,7 @@ export const About = () => {
     return (
         <div style={{textAlign:'center'}}>
             <h1>About this page</h1>
-            <p>Project is about to serach the github user</p>
+            <p>Project is about to search the github user</p>
         </div>
     )
 }
